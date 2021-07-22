5b4084bd976027a639965f74fb7b59d7e1db287c
Grace Govan <gigovan35@icloud.com>
2021-07-22 11:38:08 -0400

    Iteration 1

---
+++ Pods/Target Support Files/Pods-FirebasePractice-FirebasePracticeUITests/Pods-FirebasePractice-FirebasePracticeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FirebasePractice_FirebasePracticeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FirebasePractice_FirebasePracticeUITests
+@end
